9532ba619cd50d0e5644d3c7a73e236f573e3b6d
Lucas Ferreira <lks_ferreira@hotmail.com>
2026-01-12 17:46:11 -0300

    :bug: fix: Migra importação do Font Awesome para o JS (corrige erro 404 de fontes na Vercel)

--- index.tsx
+++ index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Font Awesome - Importação via JS para garantir carregamento correto das fontes pelo Vite
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 import { ProvedorRepositorios } from './contextos/ContextoRepositorios';
 
